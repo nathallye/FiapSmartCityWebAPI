724bcb4f19df3f7abd2fe27d055eb3aee5cf7c5f
Nathallye Bacelar <n.ferreira.tavares@avanade.com>
2022-12-02 18:19:20 -0300

    Communication client/server

--- FiapSmartCityClient/Program.cs
+++ FiapSmartCityClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace FiapSmartCityClient
 {
@@ -8,7 +9,13 @@ namespace FiapSmartCityClient
         static void Main(string[] args)
         {
             get();
+
+            getdesserializado();
+
             post();
+            
+            postserializado();
+            
             Console.Read();
         }
 
@@ -31,7 +38,6 @@ namespace FiapSmartCityClient
                 // Imprime o conteúdo na janela Console.
                 Console.Write(conteudo.ToString());
             }
-
         }
 
         static void post()
@@ -55,7 +61,66 @@ namespace FiapSmartCityClient
             {
                 Console.WriteLine("Tipo do produto criado com sucesso");
                 Console.Write("Link para consulta: " + response.Headers.Location);
+            }
+        }
 
+        static void getdesserializado()
+        {
+            // Criando um objeto Cliente para conectar com o recurso
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+            // Execute o método Get passando a url da API e salvando o resultado 
+            // em um objeto do tipo HttpResponseMessage
+            System.Net.Http.HttpResponseMessage response =
+            client.GetAsync("https://localhost:7188/api/FiapSmartCityWebAPI/ProductType").Result;
+
+            // Verifica se o Status Code é 200
+            if (response.IsSuccessStatusCode)
+            {
+                // Recupera o conteúdo JSON retornado pela API
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                // Convertendo o conteúdo em uma lista de TipoProduto
+                List<ProductType> list =
+                    JsonConvert.DeserializeObject<List<ProductType>>(content);
+
+                // Imprime o conteúdo na janela Console
+                foreach (var item in list)
+                {
+                    Console.WriteLine("Descrição:" + item.TypeDescription);
+                    Console.WriteLine("Comercializado:" + item.Marketed);
+                    Console.WriteLine(" ========== ");
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        static void postserializado()
+        {
+            // Criando um objeto Cliente para conectar com o recurso
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+            // Conteudo do tipo de produto em JSON
+            ProductType type = new ProductType();
+            type.TypeID = 101;
+            type.TypeDescription = "Grid de Energia Solar";
+            type.Marketed = true;
+
+            var json = JsonConvert.SerializeObject(type);
+
+            // Convertendo texto para JSON StringContent 
+            StringContent content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+
+            // Execute o método POST passando a url da API 
+            // e envia o conteudo do tipo StringContent
+            System.Net.Http.HttpResponseMessage response =
+                client.PostAsync("https://localhost:7013/FiapSmartCityWebAPI", content).Result;
+
+            // Verifica que o POST foi executado com sucesso
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Tipo do produto criado com sucesso");
+                Console.Write("Link para consulta: " + response.Headers.Location);
             }
         }
     }
